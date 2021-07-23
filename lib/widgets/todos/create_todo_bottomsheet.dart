5af1b92e2d6a242233989d55e9e5b8860ab35fd1
Slimmerd <daniil_silin@mail.ru>
2021-07-23 03:14:20 +0300

    Removed unused code

--- lib/widgets/todos/create_todo_bottomsheet.dart
+++ lib/widgets/todos/create_todo_bottomsheet.dart
@@ -4,18 +4,19 @@ import 'package:habit_tracker/models/todo_model.dart';
 import 'package:habit_tracker/providers/todo_provider.dart';
 import 'package:provider/provider.dart';
 
-class CreateTodoBottomSheet extends StatefulWidget{
+class CreateTodoBottomSheet extends StatefulWidget {
   final TodoCategory todoCategory;
 
-  CreateTodoBottomSheet({Key? key, required this.todoCategory}): super(key: key);
+  CreateTodoBottomSheet({Key? key, required this.todoCategory})
+      : super(key: key);
 
   @override
   _CreateTodoBottomSheetState createState() => _CreateTodoBottomSheetState();
 }
 
 class _CreateTodoBottomSheetState extends State<CreateTodoBottomSheet> {
-
   late String _name;
+
   // late String _icon;
   // final int _color = AppColors.SelectedColor.value;
 
@@ -38,14 +39,14 @@ class _CreateTodoBottomSheetState extends State<CreateTodoBottomSheet> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
-                padding: EdgeInsets.only(top: 40, left:20, bottom: 20),
+                padding: EdgeInsets.only(top: 40, left: 20, bottom: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'New todo',
                       style: TextStyle(
-                        // color: AppColors.Graphite,
+                          // color: AppColors.Graphite,
                           fontSize: 18,
                           fontWeight: FontWeight.w400),
                     ),
@@ -83,9 +84,7 @@ class _CreateTodoBottomSheetState extends State<CreateTodoBottomSheet> {
                 margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 child: TextFormField(
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Name'
-                  ),
+                      border: OutlineInputBorder(), labelText: 'Name'),
                   onSaved: (String? value) {
                     _name = value!;
                   },
@@ -107,27 +106,20 @@ class _CreateTodoBottomSheetState extends State<CreateTodoBottomSheet> {
                             _formKey.currentState!.save();
 
                             print(_name);
-                            // print(_icon);
-                            // print(_color);
                             final newCategoryTodo = Todo(
                                 name: _name,
                                 createdTime: DateTime.now(),
-                                categoryID: widget.todoCategory.id!
-                            );
+                                categoryID: widget.todoCategory.id!);
 
-                            await Provider.of<TodoProvider>(context, listen: false).addTodo(newCategoryTodo,widget.todoCategory);
+                            await Provider.of<TodoProvider>(context,
+                                    listen: false)
+                                .addTodo(newCategoryTodo, widget.todoCategory);
 
-                            // final newHabit = Habit(
-                            //     title: inputController.text,
-                            //     createdTime: DateTime.now(),
-                            //     dayList: []
-                            // );
-                            // await Provider.of<HabitProvider>(context, listen: false).add(newHabit);
                             Navigator.pop(context);
                           },
                           child: Text('Create'),
                           style: ElevatedButton.styleFrom(
-                            // primary: AppColors.MintGreen,
+                              // primary: AppColors.MintGreen,
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(8),
                               ),
