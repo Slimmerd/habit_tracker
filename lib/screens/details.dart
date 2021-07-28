4649bb03e791026d0af276e47f239152a4edf6c6
Daniil Silin <daniil_silin@mail.ru>
2021-07-28 15:45:03 +1100

    Updated details screen consumer

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -4,7 +4,7 @@ import 'package:habit_tracker/models/habit_model.dart';
 import 'package:habit_tracker/providers/habit_provider.dart';
 import 'package:habit_tracker/widgets/habits/calendar_card/calendar_card.dart';
 import 'package:habit_tracker/widgets/habits/stats_card/stats_card.dart';
-import 'package:habit_tracker/widgets/settings_bottom_sheet.dart';
+import 'package:habit_tracker/widgets/settings_habit_bottomsheet.dart';
 import 'package:provider/provider.dart';
 
 class DetailsScreen extends StatefulWidget {
@@ -17,51 +17,51 @@ class _DetailsScreenState extends State<DetailsScreen> {
   Widget build(BuildContext context) {
     Habit habit = ModalRoute.of(context)!.settings.arguments as Habit;
 
-    return Scaffold(
-      backgroundColor: AppColors.BackgroundMainColor,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.dark,
-        backgroundColor: Colors.transparent,
-        title: Text(
-          habit.title,
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.white,
-          ),
-        ),
-        actions: [
-          Container(
-            margin: EdgeInsets.only(right: 15),
-            child: InkWell(
-              customBorder: CircleBorder(),
-              splashColor: Colors.transparent,
-              onTap: () => showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (context) => Container(
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: SettingsBottomSheet())),
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.white),
-                ),
-                child: Icon(
-                  Icons.settings,
-                  size: 24,
-                  color: Colors.white,
-                ),
+    return Consumer<HabitProvider>(
+      builder: (BuildContext context, value, Widget? child) {
+        return Scaffold(
+          backgroundColor: AppColors.BackgroundMainColor,
+          appBar: AppBar(
+            elevation: 0,
+            brightness: Brightness.dark,
+            backgroundColor: Colors.transparent,
+            title: Text(
+              habit.title,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
               ),
             ),
+            actions: [
+              Container(
+                margin: EdgeInsets.only(right: 15),
+                child: InkWell(
+                  customBorder: CircleBorder(),
+                  splashColor: Colors.transparent,
+                  onTap: () => showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      builder: (context) => Container(
+                          padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom),
+                          child: SettingsHabitBottomSheet(habit: habit,))),
+                  child: Container(
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: Icon(
+                      Icons.settings,
+                      size: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Consumer<HabitProvider>(
-        builder: (BuildContext context, value, Widget? child) {
-          return ListView(
+          body: ListView(
             padding: EdgeInsets.only(bottom: 20),
             children: <Widget>[
               CalendarCard(
@@ -74,9 +74,9 @@ class _DetailsScreenState extends State<DetailsScreen> {
               ),
               MonthStatsChart(habit),
             ],
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
