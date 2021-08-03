d787e594b78c8892cfd03ab8eb1ccbfa8189a1d8
Daniil Silin <daniil_silin@mail.ru>
2021-08-03 18:15:45 +1100

    Made showing due date row

--- lib/widgets/todos/todo_card/todo_card.dart
+++ lib/widgets/todos/todo_card/todo_card.dart
@@ -5,6 +5,7 @@ import 'package:habit_tracker/models/todo_model.dart';
 import 'package:habit_tracker/providers/todo_provider.dart';
 import 'package:habit_tracker/widgets/todos/todo_card/todo_dismissible.dart';
 import 'package:habit_tracker/widgets/todos/todo_card/todo_open_bottomsheet.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class TodoCard extends StatefulWidget {
@@ -19,6 +20,51 @@ class TodoCard extends StatefulWidget {
 }
 
 class _TodoCardState extends State<TodoCard> {
+  late String dateTest;
+
+  @override
+  void initState() {
+    super.initState();
+    dateShow();
+  }
+
+  @override
+  void didUpdateWidget(covariant TodoCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    dateShow();
+  }
+
+  void dateShow() {
+    DateTime currentDate =
+        widget.todo.due != null ? widget.todo.due! : DateTime.now();
+    DateTime _now = DateTime.now();
+    DateTime today = DateTime(_now.year, _now.month, _now.day);
+    String dateString;
+
+    DateTime currentDateDay =
+        DateTime(currentDate.year, currentDate.month, currentDate.day);
+
+    if (currentDateDay.isBefore(today.subtract(Duration(days: 7)))) {
+      dateString = DateFormat.yMMMMEEEEd().format(currentDate);
+    } else if (currentDateDay.isBefore(today)) {
+      dateString = '${currentDateDay.difference(today).inDays.abs()}d ago';
+    } else if (currentDateDay.isAtSameMomentAs(today)) {
+      dateString = "Today";
+    } else if (currentDateDay.isAtSameMomentAs(today.add(Duration(days: 1)))) {
+      dateString = "Tomorrow";
+    } else if (currentDateDay.isAfter(today.add(Duration(days: 1))) &&
+        currentDateDay.difference(today).inDays < 7) {
+      dateString = DateFormat.E().format(currentDate);
+    } else if (currentDateDay.isAfter(today.add(Duration(days: 7))) &&
+        currentDateDay.difference(today).inDays < 14) {
+      dateString = '${currentDateDay.difference(today).inDays}d left';
+    } else {
+      dateString = DateFormat('d/M').format(currentDate);
+    }
+
+    dateTest = dateString;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -101,7 +147,7 @@ class _TodoCardState extends State<TodoCard> {
               ///On Russian show 14 дн
               Flexible(
                 flex: 2,
-                child: Text('Today',
+                child: Text(dateTest,
                     softWrap: false,
                     style: TextStyle(color: AppColors.MainText, fontSize: 12)),
               ),
